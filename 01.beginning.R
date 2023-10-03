dfa111b1beac51082682d76fa29e80f889523964
ElisabettaMinguzzi <146854404+ElisabettaMinguzzi@users.noreply.github.com>
2023-10-03 17:55:27 +0200

    Update 01.beginning.R

--- 01.beginning.R
+++ 01.beginning.R
@@ -14,3 +14,20 @@ final
 
 final^2
 
+#array
+sophi <- c(10,20,30,50,70) # microplastics 
+
+paula <- c(100,500,600,1000,2000) # people
+
+plot(paula, sophi)
+
+plot(paula, sophi, xlab="number of people", ylab="microplastics") 
+
+people <- paula
+microplastics <- sophi
+
+plot(people, microplastics)
+plot (people, microplastics, pch=19)
+
+plot (people, microplastics, pch=19, cex=2)
+plot (people, microplastics, pch=19, cex=2, col="blue")
