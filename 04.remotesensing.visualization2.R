78d48e9a7a236c50438cf3d072a1c263d6907244
ElisabettaMinguzzi <146854404+ElisabettaMinguzzi@users.noreply.github.com>
2023-11-02 17:23:28 +0100

    Update 04.remotesensing.visualization2.R

--- 04.remotesensing.visualization2.R
+++ 04.remotesensing.visualization2.R
@@ -9,3 +9,24 @@ im.list
 # sentinel has images of the whole planet with a 10ms resolution. The images have different bands (one for red, one for blue, one for green, one for infrared among the others)
 b2 <- im.import("sentinel.dolomites.b2.tif")
 b2    # to get the informations of the layer 
+
+cl <- colorRampPalette (c("black", "grey", "light grey")) (100)
+plot(b2, col=cl)
+# let's do the same with b3, b4 and b8
+
+# Comparing all the images in a multiframe 
+par(mfrow = c(2,2))
+plot(b2,col=cl)
+plot(b3, col=cl)
+plot(b4, col=cl)
+plot(b8, col=cl)
+
+# stack images 
+stacksent <- c(b2, b3, b4, b8)
+stacksent 
+plot(stacksent, col=cl)
+dev.off()
+plot(stacksent[[4]], col=cl)
+
+
+
