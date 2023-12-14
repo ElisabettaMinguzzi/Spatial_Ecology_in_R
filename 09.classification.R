dd37ea6ad927f9c3b8a9eeccc9c8d4bca02e9f9a
ElisabettaMinguzzi <146854404+ElisabettaMinguzzi@users.noreply.github.com>
2023-12-14 16:30:09 +0100

    Update 09.classification.R

--- 09.classification.R
+++ 09.classification.R
@@ -45,9 +45,11 @@ tabout
 
 library(ggplot2)
 # final output 
-p1 <- ggplot(tabout, aes(x=class, y=y1992, color=class) + geom_bar(stat="identity", fill="white")
+p1 <- ggplot(tabout, aes(x=class, y=y1992, color=class) + geom_bar(stat="identity", fill="white") + ylim(c(0,100))
+p2 <- ggplot(tabout, aes(x=class, y=y2006, color=class) + geom_bar(stat="identity", fill="white") + ylim(c(0,100))
+p1 + p2
 
-
+        
 
 
 
