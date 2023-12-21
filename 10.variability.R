4b887d465adfbd780af3ebe7bdf95e4503cc52e3
ElisabettaMinguzzi <146854404+ElisabettaMinguzzi@users.noreply.github.com>
2023-12-21 16:51:38 +0100

    Update 10.variability.R

--- 10.variability.R
+++ 10.variability.R
@@ -22,6 +22,20 @@ plot(nir)
 sd3 <- focal(nir, matrix(1/9,3,3),fun=sd)   # The matrix is composed of 9 pixels from 1 to 9, and they are distributed 3 by 3 (3,3); the function we're going to calculate is the standard deviation(sd)
 plot(sd3)
 
-viridisc <- colorRampPalette(viridis(7))(255)  # 255 is the amount of different colors used
-plot(sd3, col=viridisc)
+viridisc <- colorRampPalette(viridis(7))(255)  # 255 is the amount of different nuances of the 7 colors used.
+plot(sd3, col=viridisc)     # the part with the highest variability is the upper left
 
+# let's calculate the variability in a 7x7 pixels moving window
+sd7 <- focal(nir, matrix(1/49, 7,7), fun=sd)
+plot(sd7, col=viridisc)
+
+# Exercise: plot via par(mfrow()) the 3x3 and the 7x7 standard deviation
+par(mfrow=c(1,2))
+plot(sd3, col=viridisc) 
+plot(sd7, col=viridisc)    # if we enlarge the moving window (49 pixels instead of 9), we have a higher variability. 
+
+# original image plus the 7x7 sd
+im.plotRGB(sent, r=1, g=2, b=3) 
+plot(sd7, col=viridisc)    # in the upper left part we can see a lot of white, which indicates a high geological variability (or species variability in some cases), so it discriminates between snow/clouds and bare soil. 
+
+# How can we decide which layer we should apply/calculate the standard deviation to? With multivariate analysis.
