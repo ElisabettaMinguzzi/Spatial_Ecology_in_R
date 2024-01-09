0fcc41d0ff53b63ab4fe6dd90520b1ab9a0d056b
ElisabettaMinguzzi <146854404+ElisabettaMinguzzi@users.noreply.github.com>
2024-01-09 17:44:14 +0100

    Create 12.RMarkdownnew.R

---
+++ 12.RMarkdownnew.R
@@ -0,0 +1,21 @@
+---
+title: "Markdown Report"
+author: "Elisabetta Minguzzi"
+date: "2024-01-09"
+output: html_document
+---
+
+# My first markdown document!
+
+Here you can add some text if you want!
+# Knit in HTML on R --> save file
+
+In this case you attain a result:
+```{r, eval=TRUE}    
+2 + 3
+```
+
+In this case, you do not:
+```{r,eval=FALSE}
+2 + 3
+```
